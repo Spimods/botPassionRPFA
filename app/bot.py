884c21593bfe180dcac9e5114e2b8a6c88387e6f
Spimods <132432388+Spimods@users.noreply.github.com>
2024-05-01 16:23:50 +0200

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -10,12 +10,12 @@ import asyncio
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="/", intents=intents)
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="passionrp"
-)
+#conn = mysql.connector.connect(
+#    host="localhost",
+#    user="root",
+#    password="",
+#    database="passionrp"
+#)
 
 async def update_presence():
     activity = discord.Activity(
@@ -29,37 +29,37 @@ async def update_presence():
 async def on_ready():
     print(f'Connecté en tant que {bot.user}')
     await bot.change_presence(activity=discord.Game(name="Passion RP FA", type=discord.ActivityType.playing))
-    check_updates.start()
+#    check_updates.start()
     await bot.tree.sync()
     await update_presence()
 
-@tasks.loop(minutes=0.2) 
-async def check_updates():
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM contact WHERE est_mise_a_jour = 0")
-    new_updates = cursor.fetchall()
-    print('MAJ SERVER')
-    if new_updates:
-        for update in new_updates:
-            embed = discord.Embed(title=f"Message de {update[4]}", color=0x808080)
-            embed.add_field(name=f"Sujet : {update[5]}", value=f"{update[1]}, \nEMAIL : {update[6]}", inline=False)
-            tz = pytz.timezone('Europe/Paris')
-            heure_actuelle = datetime.datetime.now(tz)
-            formatted_time = heure_actuelle.strftime("le %d/%m à %H:%M:%S")
-            embed.set_footer(text=formatted_time)
-            embed.set_author(name="Passion RP FA", icon_url="https://cdn.discordapp.com/avatars/1161596570261798934/50d2afec0afdac59fef300291a75f892?size=1024")
-            embed.add_field(name="Supprimer", value="Cliquez sur 🔴 pour Effacer.")
-            embed.add_field(name="Valider", value="Cliquez sur 🟢 pour Confirmer.")
-            embed.add_field(name="En cours", value="Cliquez sur 🟠 pour : En cours.")
-            embed.add_field(name="À faire", value="Cliquez sur ⚪ pour : À faire.")
-            channel = bot.get_channel(1129024097846829177)
-            message1 = await channel.send(embed=embed)
-            await message1.add_reaction('🔴')
-            await message1.add_reaction('🟢') 
-            await message1.add_reaction('🟠') 
-            await message1.add_reaction('⚪') 
-        cursor.execute("UPDATE contact SET est_mise_a_jour = 1 WHERE est_mise_a_jour = 0")
-    conn.commit()
+#@tasks.loop(minutes=0.2) 
+#async def check_updates():
+#    cursor = conn.cursor()
+#    cursor.execute("SELECT * FROM contact WHERE est_mise_a_jour = 0")
+#    new_updates = cursor.fetchall()
+#    print('MAJ SERVER')
+#    if new_updates:
+#        for update in new_updates:
+#            embed = discord.Embed(title=f"Message de {update[4]}", color=0x808080)
+#            embed.add_field(name=f"Sujet : {update[5]}", value=f"{update[1]}, \nEMAIL : {update[6]}", inline=False)
+#           tz = pytz.timezone('Europe/Paris')
+#            heure_actuelle = datetime.datetime.now(tz)
+#            formatted_time = heure_actuelle.strftime("le %d/%m à %H:%M:%S")
+#            embed.set_footer(text=formatted_time)
+#            embed.set_author(name="Passion RP FA", icon_url="https://cdn.discordapp.com/avatars/1161596570261798934/50d2afec0afdac59fef300291a75f892?size=1024")
+#            embed.add_field(name="Supprimer", value="Cliquez sur 🔴 pour Effacer.")
+#            embed.add_field(name="Valider", value="Cliquez sur 🟢 pour Confirmer.")
+#            embed.add_field(name="En cours", value="Cliquez sur 🟠 pour : En cours.")
+#            embed.add_field(name="À faire", value="Cliquez sur ⚪ pour : À faire.")
+#            channel = bot.get_channel(1129024097846829177)
+#            message1 = await channel.send(embed=embed)
+#            await message1.add_reaction('🔴')
+#            await message1.add_reaction('🟢') 
+#            await message1.add_reaction('🟠') 
+#            await message1.add_reaction('⚪') 
+#        cursor.execute("UPDATE contact SET est_mise_a_jour = 1 WHERE est_mise_a_jour = 0")
+#    conn.commit()
 
 
 @bot.event
@@ -135,9 +135,9 @@ async def slash_command(ctx: discord.interactions.Interaction):
     await ctx.response.send_message("Passion RP FA, c'est tout simplement de l'amour: https://discord.gg/passionrp") 
 
 
-@bot.event
-async def on_disconnect():
-    conn.close()
+#@bot.event
+#async def on_disconnect():
+#    conn.close()
 
 
 bot.run(process.env.TOKEN)
